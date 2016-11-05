0f4a0efb8ed3e13adebec8d2cada75151e690d02
Daniel Palomino <dapalominop@gmail.com>
2016-11-04 23:51:00 -0500

    Integración con MongoDB y creación de tabla Producto.

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 var express = require('express');
+var mongoose = require('mongoose');
+
 var app = express();
+
+mongoose.connect("mongodb://localhost/db_test");
+
+//Definir el schema de nuestro productos
+var productSchema = {
+	title: String,
+	description: String,
+	imageUrl: String,
+	pricing: Number
+};
+
+var Product = mongoose.model("Product", productSchema)
+
 
 app.set('view engine', 'pug')
 app.use(express.static("public"));
 
 app.get('/', function (req, res) {
+  var data = {
+ 	title: "Mi primer súper producto",
+	description: "Una mega super compra",
+	imageUrl: "data.png",
+	pricing: 10
+  };
+
+  var product = new Product(data);
+  product.save(function(err){
+		console.log(product);
+  });
+
   res.render('index', { title: 'Hey', message: 'Hello there!' })
 })
 
